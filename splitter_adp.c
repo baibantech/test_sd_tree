e99410b9cd6f83ad0b9ee3516a2af8252d89bcab
lijiyong0303 <lijiyong0303@outlook.com>
2017-03-24 00:24:48 -0700

    add splitter adp file

---
+++ splitter_adp.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: splitter_adp.c
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Fri 24 Mar 2017 12:18:58 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+char *tree_get_key_from_data(char *pdata)
+{
+	return pdata;
+}
+
